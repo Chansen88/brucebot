1920ee612b1c0814ef53dec69b591426d55709cb
Chris Hansen <chansen88@me.com>
2018-03-02 17:40:37 -0800

    added primary key

--- models/index.js
+++ models/index.js
@@ -2,18 +2,18 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const message = {
-  attachments: Sequelize.ARRAY(Sequelize.TEXT),
-  avatar_url: Sequelize.STRING,
-  created_at: Sequelize.INTEGER,
-  group_id: Sequelize.STRING,
-  id: Sequelize.STRING,
-  name: Sequelize.STRING,
-  sender_id: Sequelize.STRING,
-  sender_type: Sequelize.STRING,
-  source_guid: Sequelize.STRING,
-  system: Sequelize.BOOLEAN,
-  text: Sequelize.STRING(1234),
-  user_id: Sequelize.STRING
+    id: { type: Sequelize.STRING, primaryKey: true },
+    attachments: Sequelize.ARRAY(Sequelize.TEXT),
+    avatar_url: Sequelize.STRING,
+    created_at: Sequelize.INTEGER,
+    group_id: Sequelize.STRING,
+    name: Sequelize.STRING,
+    sender_id: Sequelize.STRING,
+    sender_type: Sequelize.STRING,
+    source_guid: Sequelize.STRING,
+    system: Sequelize.BOOLEAN,
+    text: Sequelize.STRING(1234),
+    user_id: Sequelize.STRING
 };
 
 exports.Message = sequelize.define("message", message, { timestamps: true });

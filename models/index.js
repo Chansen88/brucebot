d65aaeb8cab3407b8e4394eebda51558379211f7
Chris Hansen <chansen88@me.com>
2018-05-19 11:21:06 -0700

    revert

--- models/index.js
+++ models/index.js
@@ -3,17 +3,17 @@ const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const message = {
     id: { type: Sequelize.STRING, primaryKey: true },
-    attachments: { type: Sequelize.ARRAY(Sequelize.TEXT) },
-    avatar_url: { type: Sequelize.STRING },
-    created_at: { type: Sequelize.INTEGER },
-    group_id: { type: Sequelize.STRING },
-    name: { type: Sequelize.STRING },
-    sender_id: { type: Sequelize.STRING },
-    sender_type: { type: Sequelize.STRING },
-    source_guid: { type: Sequelize.STRING },
-    system: { type: Sequelize.BOOLEAN },
-    text: { type: Sequelize.STRING(1234) },
-    user_id: { type: Sequelize.STRING }
+    attachments: Sequelize.ARRAY(Sequelize.TEXT),
+    avatar_url: Sequelize.STRING,
+    created_at: Sequelize.INTEGER,
+    group_id: Sequelize.STRING,
+    name: Sequelize.STRING,
+    sender_id: Sequelize.STRING,
+    sender_type: Sequelize.STRING,
+    source_guid: Sequelize.STRING,
+    system: Sequelize.BOOLEAN,
+    text: Sequelize.STRING(1234),
+    user_id: Sequelize.STRING
 };
 
 const Message = sequelize.define("message", message, { timestamps: true });
